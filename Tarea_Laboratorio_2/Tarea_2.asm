a0d0d36ae566a60c32f03bab6f81eb658dc3058b
gabrielegr <00199518@uca.edu.sv>
2021-04-17 22:04:30 -0600

    Laboratorio 2

---
+++ Tarea_Laboratorio_2/Tarea_2.asm
@@ -0,0 +1,26 @@
+org    200h
+
+	section    .text
+
+	;Asignando caracteres 
+	mov     byte[200h], "G"
+	mov     byte[201h], "E"
+	mov     byte[202h], "G"
+	mov     byte[203h], "R"
+
+	;Copiando valor de 200h a AX usando direccionamiento directo.
+	mov        AX, [200h]
+
+	;Copiando valor de 201h a CX usando direccionamiento indirecto por registro.
+	mov     BX, [201h]
+	mov     CX, BX
+
+	;Copiando valor de 202h a DX usando direccionamiento indirecto base más índice.
+	mov     BP, 200h
+	mov     SI, 002h
+	mov     DX, [BP+SI]
+
+	;Copiando valor de 203h a DI usando direccionamiento relativo por registro.
+	mov     DI, [BP+003h]
+
+	int    20h
